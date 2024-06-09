b01d614c55f2fcff0c91a8c94cfb781bc9345c83
Shimikoi <shimikoiku@outlook.com>
2024-06-09 13:44:33 +0800

    更新了架构，不依赖第三方库，使其更顺利地运行

--- ClassicByte.App.USBHelper/Program.cs
+++ ClassicByte.App.USBHelper/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,6 +9,11 @@ namespace ClassicByte.App.USBHelper
 {
     internal static class Program
     {
+
+        internal static String TargetPath { get; set; }
+
+        internal static String OutPutPath { get; set; }
+
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -16,7 +22,96 @@ namespace ClassicByte.App.USBHelper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Runner runner = new Runner(OnArrive);
+            StartListener(runner);
             Application.Run(new MainForm());
+        }
+
+        private static void UsbWatcher_DeviceArrival(object sender, UsbWatcher.UsbEventArgs e)
+        {
+            MessageBox.Show(e.ToString());
+        }
+
+        /// <summary>
+        /// 当事件发生时执行的委托
+        /// </summary>
+        internal delegate void Runner(EventArrivedEventArgs e);
+
+
+        /// <summary>
+        /// 查询所有设备的插拔事件
+        /// </summary>
+        /// <param name="runner"></param>
+        internal static void StartListener(Runner runner)
+        {
+            try
+            {
+                //查询所有设备的插拔事件
+                #region 第一种查询方法
+                //Win32_DeviceChangeEvent  Win32_VolumeChangeEvent
+                ManagementEventWatcher watcher = new ManagementEventWatcher();
+                WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_DeviceChangeEvent  WHERE EventType = 2 or EventType = 3");
+                watcher.EventArrived += (s, e) =>
+                {
+                    runner(e);
+
+                };
+                watcher.Query = query;
+                watcher.Start();
+                #endregion
+
+                #region 第二种查询方法
+                //// Full query string specified to the constructor
+                //WqlEventQuery q = new WqlEventQuery("SELECT * FROM Win32_ComputerShutdownEvent");
+
+                //// Only relevant event class name specified to the constructor
+                //// Results in the same query as above.
+                //WqlEventQuery query = new WqlEventQuery("Win32_ComputerShutdownEvent");
+
+                //Console.WriteLine(query.QueryString);
+
+                //ConnectionOptions connectionOptions = new ConnectionOptions();
+                //connectionOptions.EnablePrivileges = true;//启用用户特权
+
+                //ManagementScope managementScope = new ManagementScope("root\\CIMV2", connectionOptions);
+
+                //WqlEventQuery wqlEventQuery = new WqlEventQuery();
+                //wqlEventQuery.EventClassName = "Win32_DeviceChangeEvent";
+                //wqlEventQuery.Condition = "EventType = 2 or EventType = 3";
+                //wqlEventQuery.WithinInterval = TimeSpan.FromSeconds(1);
+
+                //ManagementEventWatcher watcher = new ManagementEventWatcher(managementScope, wqlEventQuery);
+                ////watcher.EventArrived += Watcher_EventArrived;
+                //watcher.EventArrived += (sender, e) =>
+                //{
+                //    var txt = "";
+                //    foreach (var p in e.NewEvent.Properties)
+                //    {
+                //        txt = "name " + p.Name + " val " + p.Value + "\r\n";
+                //        Console.WriteLine(txt);
+                //        DeviceManage.Instance.FindDevice();
+                //    }
+                //};
+                //watcher.Start();
+
+                #endregion
+
+                //ServicesManager.Instance.StartServices();
+                //Thread.CurrentThread.IsBackground = false;
+                //Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void OnArrive(EventArrivedEventArgs e)
+        {
+            #region 识别U盘
+
+
+            #endregion
         }
     }
 }
