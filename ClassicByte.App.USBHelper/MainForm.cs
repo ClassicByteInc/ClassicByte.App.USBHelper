8a0b829072fc60fb978ecd0b1e7175a6fa4e81d3
Higashitani Yume <shimikoiku@outlook.com>
2024-06-09 19:44:03 +0800

    优化了架构，新增了功能

--- ClassicByte.App.USBHelper/MainForm.cs
+++ ClassicByte.App.USBHelper/MainForm.cs
@@ -9,6 +9,7 @@ namespace ClassicByte.App.USBHelper
         public MainForm()
         {
             InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
         }
 
         private void notifyIcon1_DoubleClick(object sender, EventArgs e)
@@ -25,6 +26,29 @@ namespace ClassicByte.App.USBHelper
         {
             this.Visible = false;
             e.Cancel = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.SelectedPath = OutPutPathBox.Text;
+            folderBrowser.Description = "请选择输出的目录";
+            //folderBrowser.ShowNewFolderButton = true;
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                this.OutPutPathBox.Text = folderBrowser.SelectedPath;
+            }
+
+        }
+
+        private void tableLayoutPanel5_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
